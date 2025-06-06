589104eb4531b511c112a75736614a55bb6b8d32
Juste <juste.bukinaite@gmail.com>
2025-06-06 11:54:15 +0200

    Last of the implementation

---
+++ Assets/Scripts/LevelNavigation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelNavigation : MonoBehaviour
+{
+    public int nextSceneIndex;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
